09f2b5efb13558dfe9dcf34856dcda8e43cafe49
T-wcs <70718793+T-wcs@users.noreply.github.com>
2020-12-01 21:57:03 +0100

    Update icacls.py
    Add class

--- modules/permission/icacls.py
+++ modules/permission/icacls.py
@@ -1,39 +1,40 @@
 #!/usr/bin/python
 #coding:utf-8
-import os, sys, time
-# Get the current username
-usr = os.environ["USERNAME"]
-# Function to get full permission on powershell process windows
-def pshll():
-    procs = ['powershell.exe', 'powershell_ise.exe']
-    for proc in procs:
-        ica = "\\windows\\system32\\icacls.exe \\Windows\\System32\\WindowsPowerShell\\v1.0\\%s /grant %s:F " %(proc, usr)
-        os.system(ica)
-# Function to get full permission on main process windows
-def sys32():
-    procs = ['cmd.exe', 'smartscreen.exe', 'taskmgr.exe']
-    for proc in procs:
-        ica = "\\windows\\system32\\icacls.exe \\Windows\\System32\\%s /grant %s:F " %(proc, usr)
-        os.system(ica)
-# Function to get full permission on Explorer of Windows
-def exp():
-    ice = "\\Windows\\system32\\icacls.exe \\Windows\\explorer.exe /grant %s:F" %(usr)
-    os.system(ice)
-# Function to get full permission on Register Windows
-def reg():
-    icr = "\\Windows\\system32\\icacls.exe \\Windows\\regedit.exe /grant %s:F" %(usr)
-    os.system(icr)
-# Function to get full permission on PerfLogs directory
-def perf():
-    icp = "\\windows\\system32\\icacls.exe \Perflogs /grant %s:F" %(usr)
-    os.system(icp)
-# Function to get full permission on PerfLogs directory and more
-def perfall():
-    icp1 = "\\windows\\system32\\icacls.exe \Perflogs\* /grant %s:F" %(usr)
-    os.system(icp1)
-    icpp = "\\windows\\system32\\icacls.exe \Perflogs\gui_counter\* /grant %s:F" %(usr)
-    os.system(icpp)
-# Function to get full permission on System directory Windows
-def sysDir():
-    ic = "\\Windows\\system32\\icacls.exe \\Windows\\System /grant %s:F" %(usr)
-    os.system(ic)
+import os
+# VARIABLE ENVIRONMENT
+letter_drive_path = os.environ["SystemDrive"]
+get_usr_path      = os.environ["USERNAME"]
+
+class GetPermission():
+    """ Classe définissant les permissions de lecture, écriture sur les processus et répertoire"""
+    def __init__(self):
+        self.powershell_command = ['powershell.exe', 'powershell_ise.exe']
+        self.windows_processus  = ['explorer.exe', 'notepad.exe']
+        self.grant_permission   = "/grant {}:F".format(get_usr_path)
+        self.grant_processus    = ['cmd.exe', 'taskmgr.exe', 'smartscreen.exe']
+        self.directory_check    = ["{} \\Perflogs {}", "{} \\Perflogs\\* {}", "{} \\Perflogs\\gui_counter\\* {}", "{} \\Windows\\System {}"]
+        self.processus_exec     = "START /MIN {}\\Windows\\System32\\icacls.exe".format(letter_drive_path)
+        self.directory3         = "{}\\Windows\\System32\\WindowsPowerShell\\v1.0\\".format(letter_drive_path)
+        self.directory2         = "{}\\Windows\\System32\\".format(letter_drive_path)
+        self.directory          = "{}\\Windows\\".format(letter_drive_path)
+    # Function to get full permission on the powershell, cmd and the taskmanager
+    def proc(self):
+        for proc in self.powershell_command:
+            self.powershell_command = proc
+            self.command_execute = "{} {}{} {}".format(self.processus_exec, self.directory3, self.powershell_command, self.grant_permission)
+            os.system(self.command_execute)
+
+        for proc in self.grant_processus:
+            self.grant_processus = proc
+            self.command_execute = "{} {}{} {}".format(self.processus_exec, self.directory2, self.grant_processus, self.grant_permission)
+            os.system(self.command_execute)
+
+        for proc in self.windows_processus:
+            self.windows_processus = proc
+            self.command_execute = "{} {}{} {}".format(self.processus_exec, self.directory, self.windows_processus, self.grant_permission)
+            os.system(self.command_execute)
+    # Function to gell full permission on the directory
+    def dir(self):
+        for i in self.directory_check:
+            self.new_var = i.format(self.processus_exec, self.grant_permission)
+            os.system(self.new_var)
