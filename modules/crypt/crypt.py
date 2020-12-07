fe0f412788c2fb06dafdaaad29a3f93e1d9ca2ff
T-wcs <70718793+T-wcs@users.noreply.github.com>
2020-12-07 16:12:28 +0100

    Update crypt.py

--- modules/crypt/crypt.py
+++ modules/crypt/crypt.py
@@ -1,8 +1,6 @@
 #!/usr/bin/python
 #coding:utf-8
 from cryptography.fernet import Fernet
-from cryptography.hazmat.backends import default_backend
-from cryptography.hazmat.primitives import hashes
 import os, sys, socket, wget, struct, ctypes, shutil, base64
 # ENVIRONMENT VARIABLE
 usr = os.environ["USERNAME"]
