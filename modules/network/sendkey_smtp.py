d091554a59305fd4e7c27e1bb94d34b2451be554
T-wcs <70718793+T-wcs@users.noreply.github.com>
2020-12-10 16:48:20 +0100

    Update sendkey_smtp.py

--- modules/network/sendkey_smtp.py
+++ modules/network/sendkey_smtp.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python
 #coding:utf-8
-import smtplib
+import smtplib, os
 from email.mime.multipart import MIMEMultipart
 from email.mime.text import MIMEText
 from email.mime.base import MIMEBase
