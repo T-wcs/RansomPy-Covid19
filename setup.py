9ffa875f9dd9495f2fd149a40958ed22a6ee317a
T-wcs <70718793+T-wcs@users.noreply.github.com>
2020-12-12 17:31:55 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ import sys
 from cx_Freeze import setup, Executable
 
 path = sys.path
-includefiles = ['assets/image.jpg', 'assets/ransom.html']
+includefiles = ['assets/image.jpg', 'assets/ransom.html', 'assets/icon/windows-installer.ico']
 includes = []
 excludes = []
 packages = ["modules/crypt", "modules/network", "modules/permission", "modules/system"]
@@ -24,7 +24,7 @@ setup(
 	version = "2.0",
 	description = "Security check for windows host",
     options = {"build_exe": options},
-	executables = [Executable("main.py", base=base),
+	executables = [Executable("main.py", base=base, icon="assets/icon/windows-installer.ico"),
                     Executable("GuiCounter.py", base=base),
                     Executable("svchost.py", base=base)]
 )
