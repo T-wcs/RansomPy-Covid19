f823b06d366aa02fb1914935ed860f563569ee5a
T-wcs <70718793+T-wcs@users.noreply.github.com>
2020-12-13 00:46:21 +0100

    Update crypt_init.py

--- crypt_init.py
+++ crypt_init.py
@@ -1,10 +1,6 @@
 #!/usr/bin/python
 #coding:utf-8
 from cryptography.fernet import Fernet
-from cryptography.hazmat.backends import default_backend
-from cryptography.hazmat.primitives import hashes
-from cryptography.hazmat.primitives.kdf.pbkdf2 import PBKDF2HMAC
-from cryptography.fernet import Fernet
 import os, sys, socket, wget, struct, ctypes, shutil, base64
 
 """ THIS FILE IS PART OF THE FINAL EXECUTABLES """
