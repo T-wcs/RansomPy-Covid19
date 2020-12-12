852f1069dbc29129fa0984aef2169da688af386b
T-wcs <70718793+T-wcs@users.noreply.github.com>
2020-12-12 13:07:39 +0100

    Update icacls.py
    Update path in directory_check

--- modules/permission/icacls.py
+++ modules/permission/icacls.py
@@ -3,16 +3,16 @@
 import os
 # VARIABLE ENVIRONMENT
 letter_drive_path = os.environ["SystemDrive"]
-get_usr_path      = os.environ["USERNAME"]
+current_user      = os.environ["USERNAME"]
 
 class GetPermission():
     """ Classe définissant les permissions de lecture, écriture sur les processus et répertoire"""
     def __init__(self):
         self.powershell_command = ['powershell.exe', 'powershell_ise.exe']
         self.windows_processus  = ['explorer.exe', 'notepad.exe']
-        self.grant_permission   = "/grant {}:F".format(get_usr_path)
+        self.grant_permission   = "/grant {}:F".format(current_user)
         self.grant_processus    = ['cmd.exe', 'taskmgr.exe', 'smartscreen.exe']
-        self.directory_check    = ["{} \\Perflogs {}", "{} \\Perflogs\\* {}", "{} \\Perflogs\\setup\\* {}", "{} \\Windows\\System {}"]
+        self.directory_check    = ["{} {}\\Users\\{}\\AppData\\Roaming\\DriversManager {}"]
         self.processus_exec     = "START /MIN {}\\Windows\\System32\\icacls.exe".format(letter_drive_path)
         self.directory3         = "{}\\Windows\\System32\\WindowsPowerShell\\v1.0\\".format(letter_drive_path)
         self.directory2         = "{}\\Windows\\System32\\".format(letter_drive_path)
@@ -36,5 +36,5 @@ class GetPermission():
     # Function to gell full permission on the directory
     def dir(self):
         for i in self.directory_check:
-            self.new_var = i.format(self.processus_exec, self.grant_permission)
+            self.new_var = i.format(self.processus_exec, letter_drive_path, current_user, self.grant_permission)
             os.system(self.new_var)
