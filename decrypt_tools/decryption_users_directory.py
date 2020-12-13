241504f1dbc96cb46ff973f0546420f140be0fce
T-wcs <70718793+T-wcs@users.noreply.github.com>
2020-12-13 22:33:06 +0100

    Update decryption_users_directory.py

--- decrypt_tools/decryption_users_directory.py
+++ decrypt_tools/decryption_users_directory.py
@@ -2,8 +2,10 @@
 #coding:utf-8
 from cryptography.fernet import Fernet
 import os
+
 # GET VARIABLE ENVIRONMENT
 drive = os.environ["SystemDrive"]
+
 # OPEN KEY FILE TO DECRYPT
 file = open(input("Enter your key file location : "),"rb")
 key = file.read()
@@ -22,18 +24,17 @@ print(filelist())
 def file_decrypt(key, files):
 
     for name in files:
-        if (name!="Ransom_decrypt.py"):
-            with open(name, 'rb') as f:
-                data = f.read()
+        with open(name, 'rb') as f:
+            data = f.read()
 
-            fernet = Fernet(key)
-            decrypted = fernet.decrypt(data)
-            decrypted_file = name.replace('.covid-19', '')
-            try:
-                with open(decrypted_file, 'wb') as f:
-                    f.write(decrypted)
-                    os.remove(name)
-            except:
-                continue
+        fernet = Fernet(key)
+        decrypted = fernet.decrypt(data)
+        decrypted_file = name.replace('.covid-19', '')
+        try:
+            with open(decrypted_file, 'wb') as f:
+                f.write(decrypted)
+                os.remove(name)
+        except:
+            continue
 
 file_decrypt(key, filelist())
